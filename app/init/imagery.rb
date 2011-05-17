61bfcd4cab97965818a9cd5016c44facf01ef80d
Rico Sta. Cruz <rico@ambiescent.com>
2011-05-17 15:33:28 +0800

    + Support for different S3 endpoints

--- app/init/imagery.rb
+++ app/init/imagery.rb
@@ -15,6 +15,7 @@ class Imagery # :nodoc:
   if Main.s3?
     include Imagery::S3
     s3_bucket Main.s3.bucket
+    s3_host   Main.s3.host
     self.root = './tmp'
   end
 end
--- config/defaults/s3.rb
+++ config/defaults/s3.rb
@@ -2,7 +2,9 @@ if ENV['AMAZON_ACCESS_KEY_ID'] &&
    ENV['AMAZON_SECRET_ACCESS_KEY'] &&
    ENV['AMAZON_S3_BUCKET']
 
-  Main.set :s3, Hashie::Mash.new(bucket: ENV['AMAZON_S3_BUCKET'])
+  Main.set :s3, Hashie::Mash.new(
+    bucket: ENV['AMAZON_S3_BUCKET'],
+    host:   ENV['AMAZON_S3_ENDPOINT'] || 'http://s3.amazonaws.com')
 
 else
 
