adfc53e161a20f1e5da4f99884009e7bcc123822
Rico Sta. Cruz <rico@ambiescent.com>
2011-05-17 14:48:48 +0800

    ! Oops

--- config/defaults/s3.rb
+++ config/defaults/s3.rb
@@ -2,7 +2,7 @@ if ENV['AMAZON_ACCESS_KEY_ID'] &&
    ENV['AMAZON_SECRET_ACCESS_KEY'] &&
    ENV['AMAZON_S3_BUCKET']
 
-  Main.set :s3, Hashie::Mash.new(bucket: { ENV['AMAZON_S3_BUCKET'] })
+  Main.set :s3, Hashie::Mash.new(bucket: ENV['AMAZON_S3_BUCKET'])
 
 else
 
