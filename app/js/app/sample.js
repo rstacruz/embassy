24a739b7550877e35f934721613bcb5be3042dd5
Rico Sta. Cruz <rico@ambiescent.com>
2011-05-17 23:41:49 +0800

    ~ Iconify the toolbar

--- app/js/app/sample.js
+++ app/js/app/sample.js
@@ -1,5 +1,11 @@
 (function ($) {
-  $(function() {
-    $("a[role*=popup]").facebox();
+  $("a[role*=popup]").livequery(function() {
+    $(this).facebox();
+  });
+
+  $("#edit-actions [title]").livequery(function() {
+    $(this).tipTip({
+      delay: 0, edgeOffset: 5, fadeIn: 100, maxWidth: "300px", defaultPosition: "right"
+    });
   });
 })(jQuery);
